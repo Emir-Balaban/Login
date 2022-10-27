17d5a6cefcf2b211efc9b157b9b9e79716f990b8
Emir Balaban <95624033+Em1r-B61@users.noreply.github.com>
2022-10-27 10:43:14 +0300

    Add files via upload

---
+++ login_islemleri/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace login_islemleri
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con;
+        SqlDataReader dr;
+        SqlCommand com; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string password = textBox2.Text;
+            con = new SqlConnection("Data Source=DESKTOP-UR6B3MP\\SQLEXPRESS;Initial Catalog=Ornek;Integrated Security=True");
+            com = new SqlCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select*From Kullanici_Bilgi where kullanici_adi='" + textBox1.Text + "'and sifre='" + textBox2.Text + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Tebrikler Giriş Başarılı");
+                Form2 gecis = new Form2();
+                gecis.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Kullanıcı Adı ya da Şifre girişi.");
+            }
+            con.Close();
+        }
+    }
+}
